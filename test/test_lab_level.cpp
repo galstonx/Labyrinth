ca80aa019ecd530f0302dbfbbfd80d79cd08bcb7
Garrett Alston <galston@openmailbox.org>
2017-09-02 12:18:18 -0500

    Add files via upload

---
+++ test/test_lab_level.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "LabyrinthLevel.hpp"
+using namespace std;
+using namespace Labyrinth;
+
+int main() {
+  Level level1(15,15,2);
+  cout << level1 << endl << endl;
+  Level level2("level1_test_data.txt");
+  cout << level2;
+  return 0;
+}
